5211947e53df9d0fd00e66896a9a8ffc3131cc68
Brigid C <b@c.com>
2021-03-28 21:27:23 -0500

    Started the persisting of Employer and Skills. Completed the set up of AbstractEntity and Employer and Skills with proper annotations.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+	@NotBlank
+	@Valid
+	private String location;
 
+	public Employer(String location) {
+		this.location= location;
+	}
+
+	public Employer(){}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+	@Size(min=2)
+	private String skillDescription;
+
+	public Skill(String skillDescription) {
+		this.skillDescription= skillDescription;
+	}
+
+	public Skill(){}
+
+	public String getSkillDescription() {
+		return skillDescription;
+	}
+
+	public void setSkillDescription(String skillDescription) {
+		this.skillDescription = skillDescription;
+	}
 }
